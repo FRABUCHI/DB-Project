167009946d5036b91049a95293096dd8a8302893
YEON <alphasuno214@gmail.com>
2018-05-30 03:02:41 +0900

    Readme, test.py 추가 / haksa.py, views.py 수정
    Readme 파일 꼭 읽을 것.

--- haksa_db_to_view.py
+++ haksa_db_to_view.py
@@ -2,8 +2,8 @@ import pymysql
 
 def haksa_db (info) :
     conn = pymysql.connect(
-        host="localhost",
-        user = "root",
+        host="13.209.7.120",
+        user = "ubuntu",
         asswd = "1q2w3e4r",
         db = "ajou",
         charset = "utf8",
@@ -11,70 +11,24 @@ def haksa_db (info) :
     )
 
     cur = conn.cursor()
-    sentence1 = 'date'
-    sentence2 = 'info'
-    sentence3 = 'ajou.haksa'
-
+  
+    #방학
     if info == '방학':
-        numarray = [39, 72]
-        result = ''
-
-        querys = "SELECT " + sentence1 +", "+ sentence2 + " from " + sentence3 + " where " + sentence4 + " = " + numarray[0] + " or " + sentence4 + " = " + numarray[1]
-        cur.execute(querys)
-        date = cur.fetchall()
-        for row in date :
-            result = result + str(row[0]) +"    "+str(row[1])+ "\n"
-
-        conn.close()
-
-    elif info == '전체학사일정':
-        sentence1 = 'date'
-        sentence2 = 'info'
-        sentence3 = 'ajou.haksa'
-        result = ''
-
-        querys = "SELECT " + sentence1 +", "+ sentence2 + " from " + sentence3
-        cur.execute(querys)
-        date = cur.fetchall()
-        for row in date :
-            result = result + str(row[0]) +"    "+str(row[1])+ "\n"
-
         conn.close()
 
     #1학기 중간/기말/성적
     elif info == '1학기':
-        semester = '1'+'학기'
-        test = '시험'
-        point = '성적'
-        sql = 'SELECT date, info FROM ajou.haksa where (info LIKE %s and info LIKE %s) or (info LIKE %s and info LIKE %s)'
-        arg = [semester + '%', test +'%', semester + '%', point + '%']
-        cur.execute(sql,arg)
-        result = cur.fetchall()
         conn.close()
 
     #2학기 중간/기말/성적            
     elif info == '2학기':
-        semester = '2'+'학기'
-        test = '시험'
-        point = '성적'
-        sql = 'SELECT date, info FROM ajou.haksa where (info LIKE %s and info LIKE %s) or (info LIKE %s and info LIKE %s)'
-        arg = [semester + '%', test + '%', semester + '%', point + '%']
-        cur.execute(sql,arg)
-        result = cur.fetchall()
         conn.close()
 
     #1/2학기 전부
     elif info in ('중간','중간고사','기말','기말고사','성적','성적입력','성적정정','공고'):
-        test = '시험'
-        point = '성적'
-        sql = 'SELECT date, info FROM ajou.haksa where info LIKE %s and info LIKE %s'
-        arg = [test +'%', point + '%']
-        cur.execute(sql,arg)
-        result = cur.fetchall()
         conn.close()
 
-
-
-
+    #전체 학사 일정
+    elif info in ('일정')
 
     return result
--- views.py
+++ views.py
@@ -4,13 +4,13 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 import pymysql
 from . import haksa_db_to_view
-from . import haksa_sugang
+#from . import haksa_sugang
 
 
 def keyboard(request):
         return JsonResponse({
                 'type' : 'buttons',
-                'buttons' : ['학사 일정','도서 열람','동아리','아주봇']
+                'buttons' : ['학사일정','음식점','동아리','아주봇']
         })
 
 @csrf_exempt
@@ -21,17 +21,18 @@ def message(request):
         content_name = json_data['content']
         
 
-        if content_name == '학사 일정' :
+        if content_name == '학사일정' or '일정' :
                 return JsonResponse({
                         'message' : {
                                 'text' : '어떤 일정이 궁금하세요?'
                         },
                         'keyboard' : {
                                 'type' : 'buttons',
-                                'buttons' : ['시험','방학','휴일','수강신청 관련','행정신청','입학/졸업','개강','아주봇','처음으로']
+                                'buttons' : ['전체','시험','방학','휴일','수강신청 관련','행정신청','입학/졸업','개강','아주봇','처음으로']
                         }
                 })
-            
+
+        #시험 및 성적 관련 일정    
         elif content_name == '시험' :
                 return JsonResponse({
                         'message' : {
@@ -45,6 +46,17 @@ def message(request):
            
         #시험 및 성적 관련 일정
         elif content_name in ('1학기','2학기','중간','중간고사','기말','기말고사','성적','성적입력','성적정정','공고','방학'):
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
+                        }
+                })
+        #휴일 관련 일정
+        elif content_name == '휴일' or '공휴일' or '쉬는날': 
                 info = str(haksa_db_to_view.haksa_db(content_name))
                 return JsonResponse({
                         'message' : {
@@ -55,9 +67,112 @@ def message(request):
                                 'buttons' : ['아주봇','처음으로']
                         }
                 })
+        #전체 학사 일정
+        elif content_name == '전체' or '전체학사일정' or '전체일정': 
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['예비수강','수강신청','수강정정','수강신청 포기','취득학점 포기','아주봇','처음으로']
+                        }
+                })
+        
+        #입학/졸업 관련 일정
+        elif content_name == '입학/졸업': 
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['입학식(신·편입)','졸업식(학위 수여식)','아주봇','처음으로']
+                        }
+                })
 
+        #입학/졸업 관련 일정
+        elif content_name == '입학식': 
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
+                        }
+                })
+        
+        #입학/졸업 관련 일정
+        elif content_name == '졸업식': 
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
+                        }
+                })
+                
+
+        #개강 일정
+        elif content_name == '개강' or '개강날짜' or '개강일정': 
+        info = str(haksa_db_to_view.haksa_db(content_name))
+        return JsonResponse({
+                'message' : {
+                        'text' : info
+                },
+                'keyboard' : {
+                        'type' : 'buttons',
+                        'buttons' : ['예비수강','수강신청','수강정정','수강신청 포기','취득학점 포기','아주봇','처음으로']
+                }
+        })
+        
         #수강신청 관련 일정
-        elif content_name in ('1학기','2학기','계절학기','예비수강','수강신청','수강정정','수강신청 포기','취득학점 포기'):
+        elif content_name == '수강신청 관련':
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['예비수강','수강신청','수강정정','수강신청 포기','취득학점 포기','아주봇','처음으로']
+                        }
+                })
+
+        #방학 일정
+        elif content_name == '방학' or '여름방학' or '겨울방학':
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
+                        }
+                })
+        #행정신청 관련 일정
+        elif content_name == '행정신청':
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['전과','학기등록','전공/졸업 신청','아주봇','처음으로']
+                        }
+                })
+      
+        #행정신청 관련 일정
+        elif content_name == '전과' or '전과신청' or '전과 신청':
                 info = str(haksa_db_to_view.haksa_db(content_name))
                 return JsonResponse({
                         'message' : {
@@ -69,15 +184,38 @@ def message(request):
                         }
                 })
 
-        elif content_name == '도서 열람' :
+        #행정신청 관련 일정
+        elif content_name == '학기등록' or '등록' or '학기 등록':
+                info = str(haksa_db_to_view.haksa_db(content_name))
                 return JsonResponse({
                         'message' : {
-                                'text' : '무슨 책을 찾으십니까?'
+                                'text' : info
                         },
                         'keyboard' : {
-                                'type' : 'text'
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
                         }
                 })
+
+        #행정신청 관련 일정
+        #이 부분은 영준씨가 추가로 수정해도 됩니다. 분류가 많아서
+        #major = [전공신청,전공변경,전공취소]
+        #submajor = [부전공신청,부전공변경,부전공취소]
+        #elif content_name == major or submajor
+        #elif content_name == in(major,submajor)
+        #이런식으로 정리될수 있을거 같은데 파이썬 문법 찾아서 조금 더 깔끔하게 할 수 있으면 그렇게 해주세요.
+        elif content_name == in('전공/졸업 신청','졸업유예','졸업연기','졸업 유예','졸업 연기','전공 변경','전공 취소','복수전공','부전공','연계전공'):
+                info = str(haksa_db_to_view.haksa_db(content_name))
+                return JsonResponse({
+                        'message' : {
+                                'text' : info
+                        },
+                        'keyboard' : {
+                                'type' : 'buttons',
+                                'buttons' : ['아주봇','처음으로']
+                        }
+                })
+
 
         elif content_name == '동아리' :
                 return JsonResponse({
