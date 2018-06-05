1cbed24a67952ad8afe964567fb1ae91a10c817f
YEON <alphasuno214@gmail.com>
2018-06-06 01:23:19 +0900

    인기검색어/동아리/학사일정 완료

--- day_list.py
+++ day_list.py
@@ -1,59 +1,83 @@
 import pymysql
 from datetime import datetime
 
-nowHour = datetime.today().hour
-sentence1 = 'test.day'
-sentence2 = 'num'
-sentence3 = 'SQL_SAFE_UPDATES'
 
-conn = pymysql.connect(
+#반환값 없음.
+def push(keyword):
+    
+    conn = pymysql.connect(
         host="localhost",
         user = "root",
-        passwd = "dudwns",
-        db = "test",
+        passwd = "1q2w3e4r",
+        db = "ajou",
         charset = "utf8",
         use_unicode=True
     )
 
-cur = conn.cursor()
-    
-
-#반환값 없음.
-def push(keyword):
+    cur = conn.cursor()
 
     #중복 되면 카운트 올리고 중복 아닌 키워드는 삽입하고 카운트 올림
-    sql = '''
-    INSERT INTO classic_keyword_list(keyword) VALUES' + '(' + search + ')'
-    ON DUPLICATE UPDATE KEY num = num + 1
-    '''
+    sql = "INSERT INTO ajou.day_keyword_list(keyword) VALUES ('"+keyword+"')" + 'on duplicate key update num = num + 1'
     cur.execute(sql)
+
+
     #검색 한번 될때마다 튜플 정리
-    sql = 'select * from classic_keyword_list order by num'
+    sql = 'select * from day_keyword_list order by num desc'
     cur.execute(sql)
     
+    result = cur.fetchall()
+    for row in result :
+        print(row)
+    
+    conn.commit()
     conn.close()
 
 
 def pull():
+    
+    conn = pymysql.connect(
+        host="localhost",
+        user = "root",
+        passwd = "1q2w3e4r",
+        db = "ajou",
+        charset = "utf8",
+        use_unicode=True
+    )
 
+    cur = conn.cursor()
     #상위 10개 키워드 가져옴.
-    sql = 'select keyword from classic_keyword_list limit 10'
+    sql = 'select keyword from day_keyword_list limit 10'
     cur.execute(sql)
-    result = cur.fetchall()
-    for row in result:
-        print(row)
-
+    total = cur.fetchall()
     conn.close()
-
+    
+    result = '**오늘의 인기 검색어!** \n\n'
+    num = 1
+    for row in range(0,len(total)):
+        result += str(num) + ' ☞  ' + total[row][0] + '\n'
+        num +=1
+    
     return result
 
 def init() :
+    conn = pymysql.connect(
+        host="localhost",
+        user = "root",
+        passwd = "1q2w3e4r",
+        db = "ajou",
+        charset = "utf8",
+        use_unicode=True
+    )
+    nowHour = datetime.today().hour
+
+    cur = conn.cursor()
     if nowHour == 0 :
         sql = 'SET SQL_SAFE_UPDATES =0'
         cur.execute(sql)
-        sql = 'UPDATE day SET num = default'
+        sql = 'UPDATE day_keyword_list SET num = default'
         cur.execute(sql)
         conn.commit()
+
     conn.close()
 
 init()
